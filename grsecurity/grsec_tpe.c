27f39434854ca6abd2cfa121db4396cb3e78f242
HacKurx <hackurx@gmail.com>
2019-01-01 11:39:32 +0100

    grsec: Add stricter TPE mode to grsec's implementation
    Strict Trusted Path Execution: The TPE restrictions will be enforced on
    all users, _including_ root, who will be unable to (directly) execute
    anything that could be written to by any other user.
    
    Patch: 1510_grsec_tpe_strict.patch
    From: Vincent Strubel <clipos@ssi.gouv.fr>

--- grsecurity/grsec_tpe.c
+++ grsecurity/grsec_tpe.c
@@ -11,16 +11,28 @@ gr_tpe_allow(const struct file *file)
 {
 #ifdef CONFIG_GRKERNSEC
 	struct inode *inode = d_backing_inode(file->f_path.dentry->d_parent);
-	struct inode *file_inode = d_backing_inode(file->f_path.dentry);
 	const struct cred *cred = current_cred();
+#ifndef CONFIG_GRKERNSEC_TPE_STRICT
+	struct inode *file_inode = d_backing_inode(file->f_path.dentry);
 	char *msg = NULL;
 	char *msg2 = NULL;
+#endif
 
 	// never restrict root
 	if (gr_is_global_root(cred->uid))
 		return 1;
 
 	if (grsec_enable_tpe) {
+#ifdef CONFIG_GRKERNSEC_TPE_STRICT
+	if (gr_is_global_nonroot(inode->i_uid) ||
+			(gr_is_global_root(inode->i_uid) &&
+			((inode->i_mode & S_IWGRP) || (inode->i_mode & S_IWOTH)))) {
+		gr_log_str_fs(GR_DONT_AUDIT, GR_EXEC_TPE_MSG, "strict TPE", file->f_path.dentry, file->f_path.mnt);
+		return 0;
+	}
+	return 1;
+	}
+#else
 #ifdef CONFIG_GRKERNSEC_TPE_INVERT
 		if (grsec_enable_tpe_invert && !in_group_p(grsec_tpe_gid))
 			msg = "not being in trusted group";
@@ -73,6 +85,7 @@ next_check:
 		return 0;
 	}
 #endif
+#endif /* CONFIG_GRKERNSEC_STRICT */
 #endif
 	return 1;
 }
